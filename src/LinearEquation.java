fe3bffed35d2caada9495698389e504bb85ec367
Arhan Hossain <arhanh@nycstudents.net>
2022-10-18 12:33:30 -0400

    second commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -6,22 +6,28 @@ public class LinearEquation {
     private int x2;
     private int y2;
 
+
     // Object
     public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
 
     // Distance
     public double distance() {
         double getDistance = Math.sqrt((Math.pow(x2 - x1, 2)) + (Math.pow(y2 - y1, 2)));
-        return getDistance;
+        double roundDistance = roundedToHundreth(getDistance);
+        return roundDistance;
     }
 
     // Slope
     public double slope(){
        double getSlope = ((y2 - y1) / (x2 - x1));
-       return getSlope;
+       double roundSlope = roundedToHundreth(getSlope);
+       return roundSlope;
     }
-
 
     // Y-Intercept
     public double yIntercept(){
@@ -30,12 +36,36 @@ public class LinearEquation {
     }
 
     // Linear Equation
-    public String LinearEquation(){
-        String getLinearEquation = "y = " + slope() + "x + " + yIntercept();
+    public String linearEquation(){
+        int top = y2 - y1;
+        int bottom = x2 - x1;
+        String getLinearEquation = "y = " + top + "/" + bottom + "x + " + yIntercept();
         return getLinearEquation;
     }
 
+    // Coordinate for x
+    public String coordinateForX(double xValue){
+        double yValue = slope() * xValue + yIntercept();
+        double roundYValue = roundedToHundreth(yValue);
+        double roundXValue = roundedToHundreth(xValue);
+        String getCoordinateForX = "(" + roundXValue + ", " + roundYValue + ")";
+        return getCoordinateForX;
+    }
 
+    // Round method
+    public double roundedToHundreth(double number){
+        return (double) Math.round(number * 100) / 100;
+    }
+
+    // Prints out all info
+    public String lineInfo(){
+        String getLineInfo = "The two points are: " + "(" + x1 + ", " + y1 + ")" + " and " + "(" + x2 + ", " + y2 + ")" + "\n";
+        getLineInfo += "The equation of the line between these points is: " + linearEquation() + "\n";
+        getLineInfo += "The slope of this line is: " + slope() + "\n";
+        getLineInfo += "The y-intercept of this line is: " + yIntercept() + "\n";
+        getLineInfo += "The distance between the two points is: " + distance();
+        return getLineInfo;
+    }
 
 
 
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -8,8 +8,21 @@ public class LinearEquationRunner {
         System.out.println("Welcome to the Linear Equation Calculator!");
         System.out.print("Enter first coordinate: ");
         String coordinate1 = myScanner.nextLine();
-        System.out.println("Enter second coordinate: ");
+        System.out.print("Enter second coordinate: ");
         String coordinate2 = myScanner.nextLine();
+
+        int x1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf("(") + 1,coordinate1.indexOf(",")));
+        int y1 = Integer.parseInt(coordinate1.substring(coordinate1.indexOf(" ") + 1, coordinate1.indexOf(")")));
+        int x2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf("(") + 1, coordinate2.indexOf(",")));
+        int y2 = Integer.parseInt(coordinate2.substring(coordinate2.indexOf(" ") + 1, coordinate2.indexOf(")")));
+
+        LinearEquation linear1 = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(linear1.lineInfo());
+
+
+
+
+
 
 
 
