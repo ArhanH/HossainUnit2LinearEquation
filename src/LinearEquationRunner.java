c44fd12a0430e4429297e4d45b5c050aaa70d7a3
Arhan <arhanh@nycstudents.net>
2022-10-18 19:29:09 -0400

    Q

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class LinearEquationRunner {
     public static void main(String[] args){
-    Scanner myScanner = new Scanner(System.in);
+        Scanner myScanner = new Scanner(System.in);
 
-    // Welcome the user and ask for coordinates
+        // Welcome the user and ask for coordinates
         System.out.println("Welcome to the Linear Equation Calculator!");
         System.out.print("Enter first coordinate: ");
         String coordinate1 = myScanner.nextLine();
